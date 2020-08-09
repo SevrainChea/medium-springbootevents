9e9c77e6443a74d2575a19b21e5e2370acaabc1d
Sevrain <sevrain.chea@maiia.com>
2020-08-09 10:05:04 +0200

    Initial commit

---
+++ src/main/java/com/sevrainchea/springbootevents/SpringbooteventsApplication.java
@@ -0,0 +1,13 @@
+package com.sevrainchea.springbootevents;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbooteventsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbooteventsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sevrainchea/springbootevents/SpringbooteventsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sevrainchea.springbootevents;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbooteventsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
